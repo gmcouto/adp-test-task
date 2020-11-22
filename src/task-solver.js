ea7486cc783674ec5ac58d365dc1ef64fb338900
gmcouto@gmail.com <gmcouto@gmail.com>
2020-11-22 12:09:05 -0300

    subtraction feature

--- src/task-solver.js
+++ src/task-solver.js
@@ -1,2 +1,3 @@
 const addition = (left, right) => parseInt(left, 10) + parseInt(right, 10);
-module.exports = { addition };
+const subtraction = (left, right) => parseInt(left, 10) - parseInt(right, 10);
+module.exports = { addition, subtraction };
