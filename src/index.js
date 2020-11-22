e29e9de5fe5c8ca96effcf6246e387299cee80ee
gmcouto@gmail.com <gmcouto@gmail.com>
2020-11-22 16:19:23 -0300

    removed unecessary code

--- src/index.js
+++ src/index.js
@@ -2,27 +2,12 @@ const server = require('./server');
 // eslint-disable-next-line import/order
 const io = require('socket.io')(server);
 const userEvents = require('./user-events');
-const fetcher = require('./task-fetcher');
-const solver = require('./task-solver');
-const submitter = require('./task-submitter');
-
-const execute = async () => {
-  const problem = await fetcher.fetch();
-  console.log(JSON.stringify(problem));
-  const solution = solver.solve(problem);
-  console.log(JSON.stringify(solution));
-  const result = await submitter.submit(solution);
-  console.log(JSON.stringify(result));
-};
-execute().then(() => {});
 
 io.on('connection', (socket) => {
-  console.log('user connected');
   userEvents.setup(socket);
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
+  socket.on('disconnect', () => {});
 });
 
 server.listen(80);
+// eslint-disable-next-line no-console
 console.log('Server running at http://127.0.0.1:80/');
--- src/task-fetcher.js
+++ src/task-fetcher.js
@@ -1,7 +1,14 @@
+/* eslint-disable no-console */
 const axios = require('axios');
 const { getTaskUrl } = require('./adp-config');
 
 async function fetch() {
-  return (await axios.get(getTaskUrl)).data;
+  try {
+    return (await axios.get(getTaskUrl)).data;
+  } catch (error) {
+    console.error('An error ocurred while fetching a new task:');
+    console.error(error.response);
+    return { id: '-1', operation: 'An error ocurred. Try Again' };
+  }
 }
 module.exports = { fetch };
