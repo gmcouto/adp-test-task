d165c807edc57f99f7bf0a678b220ea2b466a030
gmcouto@gmail.com <gmcouto@gmail.com>
2020-11-22 13:20:46 -0300

    full example

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-const fetch = require('./task-fetcher');
+const fetcher = require('./task-fetcher');
+const solver = require('./task-solver');
+const submitter = require('./task-submitter');
 
-fetch().then((value) => {
-  console.log(JSON.stringify(value));
-});
+const execute = async () => {
+  const problem = await fetcher.fetch();
+  console.log(JSON.stringify(problem));
+  const solution = solver.solve(problem);
+  console.log(JSON.stringify(solution));
+  const result = await submitter.submit(solution);
+  console.log(JSON.stringify(result));
+};
+execute().then(() => {});
