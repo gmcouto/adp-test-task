6fff16c5a25219e4996b720f37744802633446be
gmcouto@gmail.com <gmcouto@gmail.com>
2020-11-22 12:08:18 -0300

    subtraction test

--- src/task-solver.test.js
+++ src/task-solver.test.js
@@ -7,3 +7,10 @@ test("Can solve addition problems", () => {
   const result = 5;
   expect(solver[operation](left, right)).toBe(result);
 });
+test("Can solve subtraction problems", () => {
+  const operation = "subtraction";
+  const left = 10;
+  const right = 7;
+  const result = 3;
+  expect(solver[operation](left, right)).toBe(result);
+});
