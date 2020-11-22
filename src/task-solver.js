614576a666e217cc4043e77c0238ea77c6d956d4
gmcouto@gmail.com <gmcouto@gmail.com>
2020-11-22 12:16:47 -0300

    additional operations

--- src/task-solver.js
+++
@@ -1,3 +0,0 @@
-const addition = (left, right) => parseInt(left, 10) + parseInt(right, 10);
-const subtraction = (left, right) => parseInt(left, 10) - parseInt(right, 10);
-module.exports = { addition, subtraction };
